969bd14362cf7e66dc6b707d8627ee5c064e6a4a
HyperManTT <randyram9@outlook.com>
2018-04-24 23:26:47 -0400

    Minor text fixes and added in react-router lib

--- iqp/src/UserForm/StartTestModal.js
+++ iqp/src/UserForm/StartTestModal.js
@@ -12,10 +12,10 @@ export const startTestModalBody = (
         <li>The use of calculators are allowed </li>
         <li>The test is sixty-five (65) minutes long </li>
         <li>Upon beginning the test a count down timer will start and the test will automatically close when the time is up. </li>
-        <li>If you finish before the allocated end time select "End Test Now" which is located to the <strong>top right hand corner of your screen</strong></li>
-        <li>Finishing the test earlier awards you more points, however, ensure that you are ready to finish before clicking "End Test Now"</li>
-        <li>Once you select "End Now" you will not be able to access you exam again</li>
-        <li>If you attempt to open external tabs or programs for assistance in the exam, the system will register this and your application will be voided.</li>
+        <li>If you finish before the allocated end time select "End Test" which is located to the <strong>top right hand corner of your screen</strong></li>
+        <li>Finishing the test earlier awards you more points, however, ensure that you are ready to finish before clicking "End Test"</li>
+        <li>Once you select "End Test" you will not be able to access you exam again</li>
+        <li><strong>If you attempt to open external tabs or programs for assistance in the exam, the system will register this and your application will be voided.</strong></li>
     </ul>
     <p>Good Luck!</p>
 </div>
